86ba8a1b304d4c3cbaa784c9510710b18cb2add6
Nasita-Haque <nasita.haque@gmail.com>
2018-01-05 16:02:48 -0500

    add new login component

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Files
+import Login from './login.jsx';
+
 class App extends React.Component {
 	render(){
 		return (
-			<div>Hello World></div>
+			<Login />
 		)
 			
 	}
