e7a8b97a921aa6be85457cdbd13dd56f6a849aec
Nasita-Haque <nasita.haque@gmail.com>
2018-01-08 17:51:45 -0500

    changing directory

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
+//Express
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
 
-app.get('/', () => {
+//Files
+const expressMiddleware = require('./middleware/index.js');
 
-})
+//Middleware
+expressMiddleware(app);
+
+// Frontend Route
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+app.listen(port, () => console.log('Running on 3000'));
+
+//Export App
+module.exports = app;
