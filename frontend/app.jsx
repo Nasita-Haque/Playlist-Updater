9ffdaa848903aed020d28cee5f50aa48c8c62e88
Nasita-Haque <nasita.haque@gmail.com>
2018-01-10 16:23:39 -0500

    add new component to app

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Files
+import PlaylistSearcher from './components/PlaylistSearcher.jsx';
+
 class App extends React.Component {
 	render(){
 		return (
-			<div>Hello World></div>
+			<PlaylistSearcher />
 		)
 			
 	}
