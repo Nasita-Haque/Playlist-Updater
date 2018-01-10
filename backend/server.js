2eb78493e0dc78c988dda9ddaa98917719441dfb
Nasita-Haque <nasita.haque@gmail.com>
2018-01-09 19:25:17 -0500

    correcting unexpected token error

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ const expressMiddleware = require('./middleware/index.js');
 expressMiddleware(app);
 
 // Frontend Route
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../', 'frontend/index.html'));
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(port, () => console.log('Running on 3000'));
