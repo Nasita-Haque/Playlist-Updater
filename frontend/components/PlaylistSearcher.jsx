2d9fcf87291f0059593a34a970ffbd8e12c53bd1
Nasita-Haque <nasita.haque@gmail.com>
2018-01-10 16:37:13 -0500

    add event handler for input box

--- frontend/components/PlaylistSearcher.jsx
+++ frontend/components/PlaylistSearcher.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class PlaylistSearcher extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {playlistUrl: ''}
+
+		this.handleChange = this.handleChange.bind(this)
+	}
+	handleChange(e){
+		console.log(e.target.value)
+	}
 	render(){
 		return (
 			<form>
 				<h1>Type in a playlist URL below.</h1>
-				<input className="playlistUrl"/>
+				<input className="playlistUrl" onChange={this.handleChange}/>
 			</form>
 		)
 	}
